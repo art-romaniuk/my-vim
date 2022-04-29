d9fc9792a602b2bd8852e0fd54d48b9624f77ede
Artem Romaniuk <a.romaniuk@tranzzo.com>
2022-04-29 17:24:03 +0300

    Added a config file for the Ale plugin.

---
+++ lua/plugins/ale.lua
@@ -0,0 +1,16 @@
+local let = vim.g
+
+-- let.ale_sign_error = '✘'
+-- let.ale_sign_warning = '⚠'
+vim.cmd([[
+    let g:ale_sign_error = '✗'
+    let g:ale_sign_warning = '⚠'
+    let g:ale_sign_style_error = '✗'
+    let g:ale_sign_style_warning = '⚠'
+    let g:ale_sign_info = 'ℹ'
+    let g:ale_floating_window_border = ['│', '─', '╭', '╮', '╯', '╰']
+
+
+    highlight link ALEWarning       Normal
+    highlight link ALEWarningSign      Search
+]])
