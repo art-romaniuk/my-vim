5d7b6cc56aa1f25a80e244e383a8f8f57c24eb1f
Artem Romaniuk <a.romaniuk@tranzzo.com>
2022-04-29 18:50:50 +0300

    Added icons for LSP.

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -19,5 +19,11 @@ vim.cmd([[
   nnoremap <silent> gs    <cmd>Lspsaga signature_help<CR>
 ]])
 
+-- Based on: https://github.com/neovim/nvim-lspconfig/wiki/UI-customization#change-diagnostic-symbols-in-the-sign-column-gutter
+vim.fn.sign_define('DiagnosticSignError', { text = '✗', texthl = 'DiagnosticSignError', numhl = 'DiagnosticSignError' })
+vim.fn.sign_define('DiagnosticSignHint', { text = '➔', texthl = 'DiagnosticSignHint', numhl = 'DiagnosticSignHint' })
+vim.fn.sign_define('DiagnosticSignInfo', { text =  'ℹ', texthl = 'DiagnosticSignInfo', numhl = 'DiagnosticSignInfo' })
+vim.fn.sign_define('DiagnosticSignWarn', { text =  '⚠', texthl = 'DiagnosticSignWarn', numhl = 'DiagnosticSignWarn' })
+
 require "lsp.lsp-installer"
 require "lsp.cmp"
