82d2a045db785f56e957e7de645c21de397dc149
Artem Romaniuk <a.romaniuk@tranzzo.com>
2022-06-04 12:02:39 +0300

    Updated the styles for the onedark colorscheme.

--- lua/plugins/onedark.lua
+++ lua/plugins/onedark.lua
@@ -4,7 +4,7 @@ g.onedark_termcolors = 256
 g.onedark_terminal_italics = 1
 
 g.onedark_color_overrides = {
-   black =          { gui = '#1f2224', cterm = '235', cterm16 = '0'},
+   black =          { gui = '#212527', cterm = '235', cterm16 = '0'},
    purple =         { gui = '#C678DF', cterm = '170', cterm16 = '5'},
    red =            { gui = '#E06C75', cterm = '204', cterm16 = '1'},
    dark_red =       { gui = '#BE5046', cterm = '196', cterm16 = '9'},
@@ -15,7 +15,7 @@ g.onedark_color_overrides = {
    cyan =           { gui = '#56B6C2', cterm = '38', cterm16 = '6' },
    white =          { gui = '#96a0b3', cterm = '145', cterm16 = '7' },
    visual_black =   { gui = "NONE", cterm = 'NONE', cterm16 = '0' },
-   comment_grey =   { gui = '#5C6370', cterm = '59', cterm16 = '15' },
+   comment_grey =   { gui = '#343941', cterm = '59', cterm16 = '15' },
    gutter_fg_grey = { gui = '#4B5263', cterm = '238', cterm16 = '15' },
    cursor_grey =    { gui = '#2C323C', cterm = '236', cterm16 = '8' },
    visual_grey =    { gui = '#3E4452', cterm = '237', cterm16 = '15' },
@@ -23,4 +23,3 @@ g.onedark_color_overrides = {
    special_grey =   { gui = '#3B4048', cterm = '238', cterm16 = '15' },
    vertsplit =      { gui = '#181A1F', cterm = '59', cterm16 = '15' },
 }
-
