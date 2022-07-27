dd652eb2e587768cf8e9c2134c047ee373390c3d
Artem Romaniuk <a.romaniuk@tranzzo.com>
2022-07-27 11:58:16 +0300

    The neo-tree plugin was removed.

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -35,37 +35,17 @@ packer.startup(function(use)
   -- Terminal inside vim
   use { 'voldikss/vim-floaterm' }
 
-  use {
-    's1n7ax/nvim-window-picker',
-    tag = "v1.*",
-    config = function()
-      require('plugins.window-picker')
-    end
-  }
-  use {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v2.x",
-    requires = {
-      "nvim-lua/plenary.nvim",
-      "kyazdani42/nvim-web-devicons", -- not strictly required, but recommended
-      "MunifTanjim/nui.nvim",
-      's1n7ax/nvim-window-picker',
-    },
-    config = function ()
-      require('plugins.neo-tree')
-    end
-  }
   -- Testing NeoTree file namager
-  -- use {
-  --   'kyazdani42/nvim-tree.lua',
-  --   requires = {
-  --     'kyazdani42/nvim-web-devicons',
+  use {
+    'kyazdani42/nvim-tree.lua',
+    requires = {
+      'kyazdani42/nvim-web-devicons',
 
-  --   },
-  --   config = function()
-  --     require('plugins.lua-tree')
-  --   end
-  -- }
+    },
+    config = function()
+      require('plugins.lua-tree')
+    end
+  }
 
   -- Fzf fuzzy finder
   use {
