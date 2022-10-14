79269cedf9ade03682a435715380fff4940eae9d
Artem Romaniuk <a.romaniuk@tranzzo.com>
2022-10-14 18:54:25 +0300

    Updated settings for harpoon plugin.

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -1,9 +1,9 @@
 local map = vim.keymap.set
+local ui = require("harpoon.ui")
 
 map('n', '<leader>a', require('harpoon.mark').add_file)
-map('n', '<leader>m', require('harpoon.ui').toggle_quick_menu)
+map('n', '<leader>m', ui.toggle_quick_menu)
 
-local ui = require("harpoon.ui")
 map('n', '<leader>1', function() ui.nav_file(1) end)
 map('n', '<leader>2', function() ui.nav_file(2) end)
 map('n', '<leader>3', function() ui.nav_file(3) end)
